aad0355cdbef68a795fe3f9251439644ab874e0f
John Slota <john.slota@gmail.com>
2016-02-16 20:08:47 -0700

    log in and log out capabilities

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+  def create
+    auth = request.env["omniauth.auth"]
+    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+    session[:user_id] = user.id
+    redirect_to root_url, :notice => "Signed in!"
+  end
+
+  def destroy
+    if current_user
+      session.delete(:user_id)
+    end
+    redirect_to root_url, :notice => "Signed out!"
+  end
+end
