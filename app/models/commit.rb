c732460172f1426dddd47b4aad5d803210f8d712
John Slota <john.slota@gmail.com>
2016-02-18 23:14:32 -0700

    newer bug and other message

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -9,6 +9,10 @@ class Commit
     if commits == {"message"=>"Not Found",
                    "documentation_url"=>"https://developer.github.com/v3"}
       broncos_easter_egg = ["Somehow, someway you have no comments on this repo breh, go broncos!!"]
+    elsif
+      commits == {"message"=>"Git Repository is empty.",
+                  "documentation_url"=>"https://developer.github.com/v3"}
+      broncos_easter_egg = ["This git repo is empty let's go!!"]
     else
       commits.map { |object| object["commit"]["message"] }
     end
